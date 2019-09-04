21df09b7e5fe06b3a73cea8241119c4b37199b30
Nirmal Kumar <54065508+nirmalkumar-au@users.noreply.github.com>
2019-09-04 23:51:51 +1000

    Add files via upload

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service'
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+  constructor(private route: ActivatedRoute,
+    private cartService : CartService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe( params => {
+      this.product = products[params.get('productId')];
+    });
+  }
+
+  addToCart(product){
+    window.alert('This product added into cart!!!');
+    this.cartService.addToCart(product);
+  }
+}
